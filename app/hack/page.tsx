7734fc0e61c176867627dd9b74c3a8d72335b0f4
Muhammed Ameen <ameens.in19@gmail.com>
2025-10-31 23:55:53 +0530

    Remove delay from hack page redirect

--- app/hack/page.tsx
+++ app/hack/page.tsx
@@ -4,23 +4,8 @@ import { useEffect } from 'react';
 
 export default function HackRedirect() {
   useEffect(() => {
-    // Add meta refresh for better social media compatibility
-    const metaRefresh = document.createElement('meta');
-    metaRefresh.httpEquiv = 'refresh';
-    metaRefresh.content = '3;url=https://dorahacks.io/hackathon/hack-for-good/';
-    document.getElementsByTagName('head')[0].appendChild(metaRefresh);
-
-    // Delay redirect to allow social media scrapers to read metadata
-    const timer = setTimeout(() => {
-      window.location.href = 'https://dorahacks.io/hackathon/hack-for-good/';
-    }, 3000);
-    
-    return () => {
-      clearTimeout(timer);
-      if (metaRefresh.parentNode) {
-        metaRefresh.parentNode.removeChild(metaRefresh);
-      }
-    };
+    // Immediate redirect - metadata is in layout for SEO/scrapers
+    window.location.href = 'https://dorahacks.io/hackathon/hack-for-good/';
   }, []);
 
   return (
